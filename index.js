3af222268a72d41786afadbf0129b73baedab780
Mahmud hassen <mahmudhassen729@gmail.com>
2023-05-04 12:53:40 +0000

    project is running

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.send("Hello from V-spider's server")
+})
+
+app.listen(3000, () => {
+    console.log('app running of port 3000')
+})
